76ff09a958c1f6e218a7e032c8089f8a42ef0e6c
TrIliadis <triliadis@hotmail.com>
2022-04-09 01:22:14 +0300

    Contact

---
+++ pliroforiaka_app/src/components/ContactCard.js
@@ -0,0 +1,34 @@
+import "./ContactCard.css";
+
+const ContactCard = (props) => {
+  return (
+    <div className="container">
+      <div className="card text-white bg-success my-3">
+        <div className="row g-0">
+          <div className="col-md-2">
+            <div className="ratio ratio-1x1">
+              <img src={props.imgUrl} className="noFilter img-fluid rounded-start embed-responsive embed-responsive-16by9" alt="" />
+            </div>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{props.title}</h5>
+              <p className="card-text">{props.text}</p>
+              <p className="card-text">
+                <b>MEDIA INQUIRIES</b>
+                <span className="text-muted">
+                  <br />
+                  {props.email}
+                  <br />
+                  {props.phone}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
