f168fd0daa12fdcf3515b80e7d7f71ae2be09110
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2022-04-19 19:34:49 +0300

    login form logic

--- pliroforiaka_app/src/components/views/Login.js
+++ pliroforiaka_app/src/components/views/Login.js
@@ -1,50 +1,69 @@
 import '../styles/ErrorPage.css';
 import '../styles/Formstyle.css';
-import React, { setState, useState, useEffect } from "react";
+import React from "react";
 import mainLogo from '../../resources/gardenblack.png'
 
-class Login extends React.Component {
+let currUsername = ''
+let currPassword = ''
 
-    validateFn
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const { loginUsername: username, loginPassword: password } = event.target.elements
-        const foundUser = this.validateFn(username, password)
-        if (!foundUser) {
-            this.setState({loginError: 'User Not Found'});
-        } else {
-            this.setState({loginError: ''});
-        }
+const onLoginButtonClick = (validateFn, setLoginError, setCurrentUser, changePageNameFn) => {
+    console.log(currUsername)
+    const foundUser = validateFn(currUsername, currPassword)
+    if (!foundUser) {
+        setLoginError('User Not Found')
+    } else {
+        //login logic here
+        setCurrentUser(foundUser)
+        setLoginError(undefined)
+        changePageNameFn('Home')
     }
+}
 
-    constructor (props) {
-        super(props)
-        const { validateUserFn } = props
-        this.validateFn = validateUserFn
+const toggleActiveClass = (value, classList) => {
+    if (!value) {
+        classList.remove('active')
+    } else {
+        classList.add('active')
     }
+}
+
+
+const Login = (props) => {
+    const { validateUserFn, loginError, setLoginError, setCurrentUser, changePageNameFn } = props
     
-    render () {
-        return <div className="container pt-5">
-            <form 
-                className="narrow-form d-flex flex-column align-items-center pt-5"
-                onSubmit={this.handleSubmit}
+    return <div className="container pt-5">
+        <div
+            className="narrow-form d-flex flex-column align-items-center pt-5"
+        >
+            <img className="mb-4" src={mainLogo} width="72" height="72" alt="main logo"></img>
+            <h2 className="mb-4">Please log in</h2>
+            <div className="form-outline mb-4">
+                <input type="text" id="loginUsername" name="loginUsername" className="form-control" onChange={(event) => {
+                    const { value, classList } = event.target 
+
+                    currUsername = value
+                    toggleActiveClass(value, classList)
+                }}/>
+                <label className="form-label" htmlFor="loginUsername">Username</label>
+            </div>
+            <div className="form-outline mb-4">
+                <input type="password" id="loginPassword" name="loginPassword" className="form-control" onChange={(event) => {
+                    const { value, classList } = event.target 
+                    currPassword = value
+                    toggleActiveClass(value, classList)
+                }}/>
+                <label className="form-label" htmlFor="loginPassword">Password</label>
+            </div>
+            <button 
+                className="btn hover-dark c-bg-green"
+                type="submit"
+                onClick={() => {onLoginButtonClick(validateUserFn, setLoginError, setCurrentUser, changePageNameFn)}}
             >
-                <img className="mb-4" src={mainLogo} width="72" height="72"></img>
-                <h2 className="mb-4">Please log in</h2>
-                <div className="form-outline mb-4">
-                    <input type="email" id="loginUsername" name="loginUsername" className="form-control" />
-                    <label className="form-label" htmlFor="loginUsername">Email address</label>
-                </div>
-                <div className="form-outline mb-4">
-                    <input type="text" id="loginPassword" name="loginPassword" className="form-control" />
-                    <label className="form-label" htmlFor="loginPassword">Password</label>
-                </div>
-                { this.loginError && <div className="alert alert-danger">{this.loginError}</div> }
-                <button className="btn me-2 hover-dark c-bg-green" type="submit">Login</button>
-            </form>
+                Login
+            </button>
+            { loginError && <div className="alert alert-danger mt-3">{loginError}</div> }
         </div>
-    }
+    </div>
 }
 
 export default Login;
