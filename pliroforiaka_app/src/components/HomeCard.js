f8b24b2e41534c231f2c24da992988fd51b32d4d
TrIliadis <triliadis@hotmail.com>
2022-04-27 12:45:22 +0300

    fixed home articles, link needs a fix

--- pliroforiaka_app/src/components/HomeCard.js
+++ pliroforiaka_app/src/components/HomeCard.js
@@ -1,22 +1,24 @@
 import "./HomeCard.css";
+import { motion } from "framer-motion";
 
-const HomeCard = () => {
+const HomeCard = (props) => {
   return (
-    <div className="card homeCard mx-3">
+    <div className="card myCard mx-3">
       <img
-        src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"
-        className="card-img-top"
+        src={props.article.imgUrl}
+        className="card-img-top myimg"
         alt="article"
       />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="ArticleLinkPlaceholder" className="btn btn-primary">
-          Link to Article
-        </a>
+      <div className="card-body d-flex flex-column">
+        <h5 className="card-title">{props.article.title}</h5>
+        <p className="card-text">{props.article.body}</p>
+        <motion.button
+          className="btn mt-auto hover-dark c-bg-green bottom mybtn"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.1, type: "tween" }}
+        >
+          Read more
+        </motion.button>
       </div>
     </div>
   );
